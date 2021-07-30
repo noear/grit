f93eedd723a809253ed7836c508f1fa8abf33b25
noear <noear@live.cn>
2021-07-30 14:33:38 +0800

    init

--- example/example1/src/main/java/example1/Example1App.java
+++ example/example1/src/main/java/example1/Example1App.java
@@ -1,12 +1,15 @@
 package example1;
 
 import org.noear.solon.Solon;
+import org.noear.solon.SolonBuilder;
 
 /**
  * @author noear 2021/6/1 created
  */
 public class Example1App {
     public static void main(String[] args) {
-        Solon.start(Example1App.class, args);
+        new SolonBuilder()
+                .onError(e->e.printStackTrace())
+                .start(Example1App.class, args);
     }
 }
--- example/example2/src/main/java/example2/Example2App.java
+++ example/example2/src/main/java/example2/Example2App.java
@@ -1,12 +1,14 @@
 package example2;
 
-import org.noear.solon.Solon;
+import org.noear.solon.SolonBuilder;
 
 /**
  * @author noear 2021/7/30 created
  */
 public class Example2App {
     public static void main(String[] args) {
-        Solon.start(Example2App.class, args);
+        new SolonBuilder()
+                .onError(e->e.printStackTrace())
+                .start(Example2App.class, args);
     }
 }
