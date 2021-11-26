9eb409b992f50b9265fc71664ab3e1f1696bca84
noear <noear@live.cn>
2021-11-27 00:18:20 +0800

    1.0

--- gritadmin/src/main/java/gritadmin/AdminApp.java
+++ gritadmin/src/main/java/gritadmin/AdminApp.java
@@ -1,10 +1,14 @@
 package gritadmin;
 
+import org.noear.solon.Solon;
+
 /**
  * @author noear 2021/6/24 created
  */
 public class AdminApp {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        Solon.start(AdminApp.class, args, app -> {
+            app.cfg().loadEnv("grit.");
+        });
     }
 }
--- gritdock/src/main/java/gritdock/DockApp.java
+++ gritdock/src/main/java/gritdock/DockApp.java
@@ -8,6 +8,8 @@ import org.noear.solon.Solon;
  */
 public class DockApp {
     public static void main(String[] args) {
-        Solon.start(DockApp.class, args);
+        Solon.start(DockApp.class, args, app -> {
+            app.cfg().loadEnv("grit.");
+        });
     }
 }
