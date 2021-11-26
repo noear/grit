31770915dae36766649997ebeaca906a59f14014
noear <noear@live.cn>
2021-11-27 01:47:39 +0800

    1.0

---
+++ grit.server.ui/src/main/java/org/noear/grit/server/ui/controller/ResourceGroupController.java
@@ -0,0 +1,13 @@
+package org.noear.grit.server.ui.controller;
+
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+
+/**
+ * @author noear
+ * @since 1.0
+ */
+@Mapping("/grit/resource/group")
+@Controller
+public class ResourceGroupController {
+}
