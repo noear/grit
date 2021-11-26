7c1b15ec531c509d490c0bffba74ca0fe76e7915
noear <noear@live.cn>
2021-11-26 22:54:17 +0800

    1.0

---
+++ grit.client/src/main/java/org/noear/grit/client/utils/HttpResultException.java
@@ -0,0 +1,11 @@
+package org.noear.grit.client.utils;
+
+/**
+ * @author noear
+ * @since 1.5
+ */
+public class HttpResultException extends RuntimeException {
+    public HttpResultException(String message) {
+        super(message);
+    }
+}
