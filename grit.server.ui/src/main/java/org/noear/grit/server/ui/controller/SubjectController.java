69037b286ef6424f0df63f227ccd65132c6e897b
noear <noear@live.cn>
2021-11-27 14:00:36 +0800

    1.0

---
+++ grit.server.ui/src/main/java/org/noear/grit/server/ui/controller/SubjectController.java
@@ -0,0 +1,12 @@
+package org.noear.grit.server.ui.controller;
+
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+
+/**
+ * @author noear 2021/11/27 created
+ */
+@Mapping("/grit/subject/entity")
+@Controller
+public class SubjectController {
+}
---
+++ grit.server.ui/src/main/java/org/noear/grit/server/ui/controller/SubjectGroupController.java
@@ -0,0 +1,30 @@
+package org.noear.grit.server.ui.controller;
+
+import org.noear.grit.client.GritClient;
+import org.noear.grit.model.domain.ResourceSpace;
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * @author noear 2021/11/27 created
+ */
+@Mapping("/grit/subject/group")
+@Controller
+public class SubjectGroupController extends BaseController{
+    @Mapping
+    public Object home(Integer state) throws SQLException {
+        if(state == null){
+            state = 1;
+        }
+
+        List<ResourceSpace> list = GritClient.global().resourceAdmin().getSpaceList();
+
+        viewModel.put("state", state);
+        viewModel.put("list", list);
+
+        return view("grit/ui/resource_space");
+    }
+}
