dbae97c9769cda2b37561e7b4384ebc503766cd5
noear <noear@live.cn>
2021-11-29 11:25:53 +0800

    1.0

--- gritadmin/src/main/java/gritadmin/controller/LoginController.java
+++ gritadmin/src/main/java/gritadmin/controller/LoginController.java
@@ -1,8 +1,97 @@
 package gritadmin.controller;
 
+import org.noear.solon.Solon;
+import org.noear.solon.Utils;
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.MethodType;
+import org.noear.solon.core.handle.ModelAndView;
+import org.noear.solon.core.handle.Result;
+import org.noear.water.utils.ImageUtils;
+import org.noear.water.utils.RandomUtils;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 /**
  * @author noear
  * @since 1.0
  */
-public class LoginController {
+@Controller
+public class LoginController extends BaseController {
+
+    @Mapping("/login") //视图 返回
+    public ModelAndView login() {
+        //Config.regWater(request);
+
+        return view("login");
+    }
+
+    @Mapping("/")
+    public void index() throws Exception{
+        Context.current().redirect("/login");
+    }
+    //-----------------
+
+    //ajax.path like "{view}/ajax/{cmd}"
+
+    @Mapping("/login/ajax/check")  // Map<,> 返回[json]  (ViewModel 是 Map<String,Object> 的子类)
+    public Object login_ajax_check(String userName, String passWord, String validationCode, Context ctx) throws Exception {
+
+        //验证码检查
+        if (!validationCode.toLowerCase().equals(getValidation())) {
+            return Result.failure("提示：验证码错误！");
+        }
+
+        if (Utils.isEmpty(userName) || Utils.isEmpty(passWord)) {
+            return Result.failure("提示：请输入账号和密码！");
+        }
+
+        String user0 = Solon.cfg().get("grit.admin.user", "");
+        String password0 = Solon.cfg().get("grit.admin.password", "");
+
+        boolean isLogin = (user0.equals(userName) && password0.equals(passWord));
+
+
+        if (isLogin == false)
+            return Result.failure("提示：账号或密码不对！"); //set 直接返回；有利于设置后直接返回，不用另起一行
+        else {
+            ctx.sessionSet("user", user0);
+
+            return Result.succeed("/bcf/");
+        }
+    }
+
+
+    /*
+     * 获取验证码图片
+     */
+    @Mapping(value = "/login/validation/img",method = MethodType.GET, produces = "image/jpeg")
+    public void getValidationImg(Context ctx) throws IOException {
+        // 生成验证码存入session
+        String code = RandomUtils.code(4);
+        setValidation(code);
+
+        // 获取图片
+        BufferedImage bufferedImage = ImageUtils.getValidationImage(code);
+
+        // 禁止图像缓存
+        ctx.headerSet("Pragma", "no-cache");
+        ctx.headerSet("Cache-Control", "no-cache");
+        ctx.headerSet("Expires", "0");
+
+        // 图像输出
+        ImageIO.setUseCache(false);
+        ImageIO.write(bufferedImage, "jpeg", ctx.outputStream());
+    }
+
+    public final String getValidation() {
+        return (String) Context.current().session("Validation_String");
+    }
+
+    public final void setValidation(String validation) {
+        Context.current().sessionSet("Validation_String", validation.toLowerCase());
+    }
 }
---
+++ gritadmin/src/main/java/gritadmin/dso/AppFilterImpl.java
@@ -0,0 +1,47 @@
+package gritadmin.dso;
+
+
+import org.noear.solon.Utils;
+import org.noear.solon.annotation.Component;
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.Filter;
+import org.noear.solon.core.handle.FilterChain;
+import org.noear.solon.extend.health.HealthHandler;
+
+/**
+ * 应用过滤器，增加访问控制
+ *
+ * @author noear
+ * @since 1.0
+ */
+@Component
+public class AppFilterImpl implements Filter {
+    @Override
+    public void doFilter(Context ctx, FilterChain chain) throws Throwable {
+        if (authCheck(ctx)) {
+            chain.doFilter(ctx);
+        }
+    }
+
+    private boolean authCheck(Context ctx) {
+        String path = ctx.path();
+
+        //特殊的路径通过
+        if (path.startsWith("/login") ||
+                path.startsWith("/run/") ||
+                path.startsWith("/msg/") ||
+                path.equals(HealthHandler.HANDLER_PATH)) {
+            return true;
+        }
+
+        //有会话账号能过
+        if (Utils.isNotEmpty(ctx.session("user", ""))) {
+            return true;
+        }
+
+        //否则，到登录页去
+        ctx.setHandled(true);
+        ctx.redirect("/login");
+        return false;
+    }
+}
