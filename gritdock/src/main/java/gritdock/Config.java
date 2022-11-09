391f1eb9b0dec00fe20c71bfe507b21c2eb54bed
noear <noear@live.cn>
2022-11-09 15:04:38 +0800

    1.4.2

--- gritdock/src/main/java/gritdock/Config.java
+++ gritdock/src/main/java/gritdock/Config.java
@@ -8,7 +8,7 @@ import org.noear.solon.Solon;
  * */
 public class Config {
 
-    public static final String grit_version = "v1.4.1";
+    public static final String grit_version = "v1.4.2";
 
     public static boolean enable() {
         return Solon.cfg().getBool("gritdock.enable", true);
